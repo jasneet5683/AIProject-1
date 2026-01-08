6a91f42d62e4bd243b70d5c6fbcd18181055f5cf
jasneet5683 <jasneet.happy@gmail.com>
2026-01-08 21:31:14 +0530

    Update api.py
    updated gemini model

--- api.py
+++ api.py
@@ -131,7 +131,7 @@ def chat(request: PromptRequest):
         #  INITIALIZE GOOGLE GEMINI
         # gemini-1.5-flash is fast, cheap, and has 1M token context
         llm = ChatGoogleGenerativeAI(
-            model="gemini-1.5-flash",
+            model="gemini-pro",  
             google_api_key=api_key,
             temperature=0  # 0 means be factual, not creative
         )
@@ -178,3 +178,4 @@ def chat(request: PromptRequest):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
